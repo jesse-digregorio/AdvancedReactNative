bdd61ad84a36927bdee534c1579ec94b3a89de06
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-15 18:47:15 -0700

    lecture 17 - the Pan Responder sysstem

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder
+} from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+
+    });
+
+    this.state = { panResponder };
+    // while this is convention (putting panResponder in state) according to SG,
+    // we will never call .setState on panResponder.
+    // it could just as easily be placed directly in the object like so:
+    //    this.panResponder = { panResponder };
+    // We are only putting it in state, because that is the current convention.
+    //
+  }
 
   renderCards() {
     return this.props.data.map(item => {
