16e11e65ab43885aeaf1b87c0f68c7216d66962b
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-16 22:27:51 -0700

    lectures 32 & 33 - the defaultProps systems & resetting the card position

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -11,6 +11,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
   constructor(props) {
     super(props);
 
@@ -64,8 +69,12 @@ class Deck extends Component {
     const { onSwipeRight, onSwipeLeft, data } = this.props;
     const item = data[this.state.index];
 
-    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    if (this.props.onSwipeRight) {
 
+    }
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
   }
 
   resetPosition() {
