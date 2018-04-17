279d783b743ec974faa29df45579c9d248f402f4
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-16 21:48:11 -0700

    lecture 28 & 29 - programatic animation & forcing swipes

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -8,6 +8,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -30,10 +31,10 @@ class Deck extends Component {
       // when the user removes their finger from the screen
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right');
+          this.forceSwipe('right');
         }
         else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left');
+          this.forceSwipe('left');
         }
         else {
           this.resetPosition();
@@ -51,11 +52,18 @@ class Deck extends Component {
     // same for position, added in lesson 20
   }
 
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position,
+      { toValue: { x: x, y: 0 },
+        duration: SWIPE_OUT_DURATION
+    }).start();
+  }
+
   resetPosition() {
     Animated.spring(this.state.position, {
       toValue: { x: 0, y: 0 }
     }).start();
-
   }
 
   getCardStyle() {
