fbd547ae1d112c3eb2d6652bbdd8f1efe045d856
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-17 23:16:37 -0700

    lecture 54 - creating new users

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,5 +1,20 @@
-module.exports = function(req, res) {
+const admin = require('firebase-admin');
 
-  res.send(req.body);
+module.exports = function(req, res) {
+  // verify the user provided a phone number
+  if (req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input'});
+  }
+
+  // format the phone number to remove non-numeric characters
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create a new user account using the phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
+  // respond to the user request, sa
+
 
 }
