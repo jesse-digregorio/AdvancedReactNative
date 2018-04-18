a4e4abeb33d50aa83fd18ef42f9d82e1a82a8a10
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-17 23:54:23 -0700

    lecture 55 - testing new user creation

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,20 +1,18 @@
 const admin = require('firebase-admin');
 
 module.exports = function(req, res) {
-  // verify the user provided a phone number
-  if (req.body.phone) {
-    return res.status(422).send({ error: 'Bad Input'});
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
   }
 
-  // format the phone number to remove non-numeric characters
+  // Format the phone number to remove dashes and parens
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
-  // create a new user account using the phone number
+  // Create a new user account using that phone number
   admin.auth().createUser({ uid: phone })
     .then(user => res.send(user))
     .catch(err => res.status(422).send({ error: err }));
 
-  // respond to the user request, sa
-
-
+  // Respond to the user request, saying the account was made
 }
