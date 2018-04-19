ec4c84bb6baa7a9d317c45b9218e879afb2b6058
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-19 12:27:17 -0700

    lecture 63 - invalidating used codes. this was a mess. ESLint was complaining about nested promises, which the instructor didn't touch on or have trouble with. After some reading/researching this is what I ended up with. Let's see if it works. :)

--- one-time-password/functions/verify_one_time_password.js
+++ one-time-password/functions/verify_one_time_password.js
@@ -1,11 +1,33 @@
-module.exports = function(req, res) {
-  if (!req.body.phone) {
-    return res.status(422).send({ error: 'You must provide a phone number' });
+const admin = require('firebase-admin');
+module.exports = (req, res) => {
+  if (!req.body.phone || !req.body.code) {
+    return res.status(422).send({ error: 'Both phone number and code must be provided.' });
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
-  const code = parseInt(code);
+  const code = parseInt(req.body.code);
+  const userRef = admin.database().ref(`users/${phone}`);
 
+  admin.auth().getUser(phone)
+  .then(() => userRef.once('value'))
+  .then((snapshot) => {
 
+    const user = snapshot.val();
 
-}
+    if (user.code !== code || !user.codeValid) {
+      return res.status(422).send({ error: 'Code invalid.' });
+    }
+
+    return userRef.update({ codeValid: false });
+
+  })
+  .then(() => {
+    res.send('ok');
+    return null; // Warning: ESLint: Expected to return a value at the end of an arrow function.
+  })
+  .catch((error) => {
+    res.status(422).send({ error });
+  });
+
+  return null; // Warning: ESLint: Expected to return a value at the end of an arrow function.
+};
