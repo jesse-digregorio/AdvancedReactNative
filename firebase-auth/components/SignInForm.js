2bb524e701f24eefdb8754e78069a2180cde8ae1
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-20 22:29:04 -0700

    lecture 71 - async/await on user signIn

---
+++ firebase-auth/components/SignInForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-fb7e2.cloudfunctions.net';
+const verifyOneTimePasswordUri = `${ROOT_URL}/verifyOneTimePassword`;
+
+class SignInForm extends Component {
+  state = {
+    phone: '',
+    code: '',
+    token: null;
+  };
+
+  handleSubmit = async () => {
+    try {
+      console.log(createUserURI); console.log(requestPassURI);
+      await axios.post(verifyOneTimePasswordUri,
+        {
+          phone: this.state.phone,
+          code: this.state.code
+        });
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+        <FormLabel>Code</FormLabel>
+          <FormInput
+            value={this.state.code}
+            onChangeText={phone => this.setState({ code })}
+          />
+        </View>
+        <Button onPress={this.handleSubmit} title="Sign In" />
+      </View>
+    );
+  }
+
+}
+
+export default SignInForm;
