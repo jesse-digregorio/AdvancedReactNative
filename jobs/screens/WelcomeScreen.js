26c3bbaff0976e995c0175f8014253c23226dd42
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-24 21:16:53 -0700

    lecture 91 - Styling the Welcome Screen

--- jobs/screens/WelcomeScreen.js
+++ jobs/screens/WelcomeScreen.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import Expo from 'expo';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp' },
+  { text: 'Set your location, then swipe away.'}
+];
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
+      <View style={style.viewStyle}>
+        <Slides data={SLIDE_DATA} />
       </View>
     );
+  }
+}
+
+const style = {
+  viewStyle: {
+    paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+    flex: 1
   }
 }
 
