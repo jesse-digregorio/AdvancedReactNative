49463ba05d8807853ec3b3356be0d700611dabca
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-25 23:17:38 -0700

    lecture 100 - logging in with facebook

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -1,7 +1,8 @@
 import { AsyncStorage } from 'react-native';
-
+import { Facebook } from 'expo'; //exponent?
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
 } from './types';
 
 // How to use AsyncStorage:
@@ -13,8 +14,25 @@ export const facebookLogin = () => async dispatch => {
 
   if (token) {
     // dispatch action saying fb login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+
   } else {
     // start up fb login process
-    
+    doFacebookLogin(dispatch);
   }
-}
+};
+
+const doFacebookLogin = async (dispatch) => {
+  const appId = 'application ID if I were using facebook';
+
+  let { token, type }  = await Facebook.logInWithReadPermissionsAsync(appId, {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+};
