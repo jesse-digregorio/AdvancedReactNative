4663e798414507dffb3cba843c5a85be922781f6
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-25 20:49:55 -0700

    I'll be only watching the facebook portion. I don't want to make a facebook account just to dev with fb.

--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+// The Auth Screen - I don't want to make a facebook account
+// just to do Auth via facebook. I'll be watching, but not doing,
+// the lessons.
+
 class AuthScreen extends Component {
   render() {
     return (
