c18cd2547ba103b5c4901fc0cee7836f3178505c
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-26 21:59:35 -0700

    lecture 103 - world's biggest gotcha

--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
@@ -11,6 +11,7 @@ class AuthScreen extends Component {
 
   componentDidMount() {
     this.props.facebookLogin();
+    AsyncStorage.removeItem('fb_token');
   }
 
   render() {
