e893fde28dccc3d05781fbbaa12daaca666ff9e5
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-29 22:10:45 -0700

    lecture 124 - customizing mapviews

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -10,17 +10,40 @@ import * as actions from '../actions';
 class DeckScreen extends Component {
 
   renderCard(job) {
-      return (
-        <Card title={job.title}>
-          <View style={styles.detailWrapper}>
-            <Text>{job.company}</Text>
-            <Text>{job.formattedRelativeTime}</Text>
-          </View>
-          <Text>
-            {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
-          </Text>
-        </Card>
-      );
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.045,
+      latitudeDelta: 0.02
+    }
+    return (
+      <Card title={job.title}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={}
+          >
+          </MapView>
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="No More Jobs">
+
+      </Card>
+    );
   }
 
   render() {
