7cbab044dd75cf720b7f4981b682b8c736e32615
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-05-03 21:27:45 -0700

    lecture 134 - styling the job card

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -22,11 +22,26 @@ class ReviewScreen extends Component {
 
   renderLikedJobs() {
     return this.props.likedJobs.map(job => {
-      const { company, formattedRelativeTime, url } = job;
+      const {
+          company, formattedRelativeTime, url,
+          longitude, latitude, jobtitle, jobkey
+      } = job;
+      const initialRegion = {
+        longitude,
+        latitude,
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.02
+      };
 
       return (
-        <Card>
+        <Card title={jobtitle} key={jobkey}>
           <View style={{ height: 200 }}>
+            <MapView
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android'}
+              scrollEnabled={false}
+              initialRegion={initialRegion}
+            />
             <View style={styles.detailWrapper}>
               <Text style={styles.italics}>{company}</Text>
               <Text style={styles.italics}>{formattedRelativeTime}</Text>
@@ -56,6 +71,7 @@ const styles = {
     fontStyle: 'italic'
   },
   detailWrapper: {
+    marginTop: 10,
     marginBottom: 10,
     flexDirection: 'row',
     justifyContent: 'space-around'
